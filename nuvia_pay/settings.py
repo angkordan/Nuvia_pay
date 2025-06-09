8eb70d960eaa2a74c22c20868864d9557456ea84
Seizer-12 <idowuenoch65@gmail.com>
2025-06-09 12:22:30 +0100

    Railway configuration

--- nuvia_pay/settings.py
+++ nuvia_pay/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!x!j(4w_fez-vn9aj&j6adpn9hzrr)q4^r9kz2t(qlywi#ea#6'
+SECRET_KEY_OLD = 'django-insecure-!x!j(4w_fez-vn9aj&j6adpn9hzrr)q4^r9kz2t(qlywi#ea#6'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-!x!j(4w_fez-vn9aj&j6adpn9hzrr)q4^r9kz2t(qlywi#ea#6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Railway uses environment variables
